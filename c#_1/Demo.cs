c536f05353fdefce4d84117b055c4af62572b2d0
StevenIsHereIGuess <stevenishereiguess@gmail.com>
2024-07-30 14:00:43 -0400

    today I made an animation for a checkpoint, and finished (more or less) c#

--- c#_1/Demo.cs
+++ c#_1/Demo.cs
@@ -8,10 +8,18 @@ public class Demo : MonoBehaviour
     public int myAge = 13;
 
     private string myName = "Steven";
+    string[] sentence;
     
     // Update is called once per frame
-   void Start(){
-     MyFunction();
+                                                    
+   void Awake(){
+        
+        string[] sentence = {"This is my first atempt at a game", "This is my first platformer game", "Hello this is my first game"};
+        for (int i = 0; i < sentence.Length; i++)
+          {
+            Debug.Log(sentence[i]);
+          }
+    
 
    }
 
@@ -20,4 +28,6 @@ public class Demo : MonoBehaviour
       Debug.Log("I have no idea what im doing"); //its is Debug.Log right? not Console.Write?
 
     }
+
+  
 }
