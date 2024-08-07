23d5ff84001a0188436aae826474e821865df028
StevenIsHereIGuess <stevenishereiguess@gmail.com>
2024-08-07 14:10:17 -0400

    Today I added movement mechanics to my game, so now you can dash and dive

--- 2d_game_mechanics_1/Movement.cs
+++ 2d_game_mechanics_1/Movement.cs
@@ -4,56 +4,119 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Declare the Rigidbody2D variable
     private Rigidbody2D rb;
     private BoxCollider2D coll;
 
     [SerializeField] private LayerMask jumpableGround;
     public GameObject spawnPoint;
     public float speed;
-    [SerializeField] private int numDashes;
-    [SerializeField] private int dashForce;
+    public float dashSpeed;
+    public float dashDuration;
+    public float dashCooldown;
+    public float diveSpeed;
+    public float diveAngle;
+    public float decelerationTime;
 
-    private int dashesLeft;
     private bool isGrounded;
+    private bool isDashing;
+    private bool isDiving;
+    private bool canDash = true;
+    private float dashDirection;
+    private float diveDirection;
 
-    // Start is called before the first frame update
+    private Coroutine currentCoroutine;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         coll = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
     private void Update()
     {
         float dirX = Input.GetAxis("Horizontal");
-        rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (!isDashing && !isDiving)
         {
-            rb.velocity = new Vector2(rb.velocity.x, 14f);
-        }
+            rb.velocity = new Vector2(dirX * speed, rb.velocity.y);
 
-        // Dash logic
-        if (Input.GetAxis("Horizontal") != 0 && !IsGrounded() && rb.velocity.y <= 0)
-        {
-            if (dashesLeft > 0)
+            if (Input.GetKeyDown(KeyCode.W) && IsGrounded())
             {
-                Vector3 dir = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-                rb.velocity = Vector3.zero;
-                rb.AddForce(dir.normalized * dashForce);
-                dashesLeft--;
+                rb.velocity = new Vector2(rb.velocity.x, 14f);
             }
+
+            if (Input.GetKeyDown(KeyCode.E) && canDash)
+            {
+                dashDirection = dirX != 0 ? Mathf.Sign(dirX) : transform.localScale.x; // Dash in the direction of movement or facing direction
+                StartCoroutine(Dash());
+            }
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                diveDirection = dirX != 0 ? Mathf.Sign(dirX) : transform.localScale.x; // Dive in the direction of movement or facing direction
+                StartCoroutine(Dive(diveDirection));
+            }
+        }
+        else if (isDashing && Input.GetKeyDown(KeyCode.Q))
+        {
+            StopCurrentAction();
+            diveDirection = dirX != 0 ? Mathf.Sign(dirX) : transform.localScale.x; // Dive in the direction of movement or facing direction
+            StartCoroutine(Dive(diveDirection));
+        }
+        else if (isDiving && Input.GetKeyDown(KeyCode.E) && canDash)
+        {
+            StopCurrentAction();
+            dashDirection = dirX != 0 ? Mathf.Sign(dirX) : transform.localScale.x; // Dash in the direction of movement or facing direction
+            StartCoroutine(Dash());
         }
     }
 
-    void FixedUpdate()
+    private IEnumerator Dash()
     {
-        if (IsGrounded())
+        isDashing = true;
+        canDash = false;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0;
+        rb.velocity = new Vector2(dashDirection * dashSpeed, 0);
+
+        yield return new WaitForSeconds(dashDuration);
+
+        StartCoroutine(Decelerate(dashDirection, dashSpeed));
+
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+    }
+
+    private IEnumerator Dive(float direction)
+    {
+        isDiving = true;
+        float diveVelocityX = Mathf.Cos(diveAngle * Mathf.Deg2Rad) * diveSpeed * direction;
+        float diveVelocityY = Mathf.Sin(diveAngle * Mathf.Deg2Rad) * diveSpeed;
+
+        rb.velocity = new Vector2(diveVelocityX, -diveVelocityY);
+
+        yield return new WaitForSeconds(0.5f); // Adjust the duration of the dive as needed
+
+        StartCoroutine(Decelerate(direction, diveSpeed));
+
+        isDiving = false;
+    }
+
+    private IEnumerator Decelerate(float direction, float initialSpeed)
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed < decelerationTime)
         {
-            dashesLeft = numDashes;
+            rb.velocity = new Vector2(Mathf.Lerp(initialSpeed * direction, 0, timeElapsed / decelerationTime), rb.velocity.y);
+            timeElapsed += Time.deltaTime;
+            yield return null;
         }
+
+        rb.velocity = new Vector2(0, rb.velocity.y);
     }
 
     private bool IsGrounded()
@@ -67,5 +130,16 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.position = spawnPoint.transform.position;
         }
+    }
+
+    private void StopCurrentAction()
+    {
+        if (currentCoroutine != null)
+        {
+            StopCoroutine(currentCoroutine);
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        isDashing = false;
+        isDiving = false;
     }
 }
